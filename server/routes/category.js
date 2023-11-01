3caa07d2669432f0d8217dac89e9cb2613f76556
gauravgit config --global user.emailgit config --global user.name gaurav <gaurav.kashyap@sachtechsolution.com>
2023-11-01 12:35:45 +0530

    session

--- server/routes/category.js
+++ server/routes/category.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var categoryController = require('../controllers/Category');
 var router = express.Router();
-var { verifyTokenAdmin, verifyTokenBoth } = require("../middleware/authentication");
+var { verifyTokenAdmin, verifyTokenBoth, validateCSRFToken } = require("../middleware/authentication");
 const { addToMulter } = require('../services/helper/index')
 var validate = require('../middleware/validation')
 const { addCategorySchema, addSubCategorySchema, updateCategorySchema } = require('../validations/category')
@@ -10,7 +10,7 @@ const { addCategorySchema, addSubCategorySchema, updateCategorySchema } = requir
 router.get('/getCategories', verifyTokenBoth, categoryController.getCategories);
 
 // with Admin Token routes
-router.post('/addCategory', verifyTokenAdmin, addToMulter.single('category'), validate(addCategorySchema), categoryController.addCategories);
+router.post('/addCategory', validateCSRFToken, verifyTokenAdmin, addToMulter.single('category'), validate(addCategorySchema), categoryController.addCategories);
 router.post('/updateCategory', verifyTokenAdmin, addToMulter.single('category'), validate(updateCategorySchema), categoryController.updateCategory);
 router.post('/addSubcategory', verifyTokenAdmin, addToMulter.single('subcategory'), validate(addSubCategorySchema), categoryController.addSubCategories);
 router.post('/updateSubcategory', verifyTokenAdmin, addToMulter.single('subcategory'), validate(updateCategorySchema), categoryController.updateSubcategory);
