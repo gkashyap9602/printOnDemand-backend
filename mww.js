4ebf236f11d98958353a1423c73af14111e16d64
gauravgit config --global user.emailgit config --global user.name gaurav <gaurav.kashyap@sachtechsolution.com>
2023-11-02 12:42:55 +0530

    session

--- mww.js
+++ mww.js
@@ -16,12 +16,12 @@ app.use(bodyParser.json());
 app.use(express.json({ limit: '50mb' }))
 app.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
 
-// app.use(cors({
-//   origin: ["https://mwwdev.solidappmaker.ml","solidappmaker.ml", "http://localhost:3000", "http://localhost:3002"],
-//   methods: ["POST", "PUT", "GET", "OPTIONS", "HEAD", "DELETE"],
-//   credentials: true
-// }));
-app.use(cors({ origin: "*" ,credentials:true}));
+app.use(cors({
+  origin: ["https://mwwdev.solidappmaker.ml","solidappmaker.ml", "http://localhost:3000", "http://localhost:3002"],
+  methods: ["POST", "PUT", "GET", "OPTIONS", "HEAD", "DELETE"],
+  credentials: true
+}));
+// app.use(cors({ origin: "*" ,credentials:true}));
 
 app.use(session({
   secret: 'mySecretKey',
