e795f3fde6f3e640a6a9c9ced3cd8450d04bb2ec
gauravgit config --global user.emailgit config --global user.name gaurav <gaurav.kashyap@sachtechsolution.com>
2023-11-02 11:49:36 +0530

    session

--- mww.js
+++ mww.js
@@ -24,7 +24,7 @@ app.use(session({
   saveUninitialized: true,
   cookie: {
     secure: true,
-    sameSite: 'none',
+    sameSite: 'lax',
     httpOnly: true,
     domain: "mwwdev.solidappmaker.ml",
     path: "/",
