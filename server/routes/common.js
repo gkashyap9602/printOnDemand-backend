f30bde46f0bef730e75cc90717eeedf9359aa574
gauravgit config --global user.emailgit config --global user.name gaurav <gaurav.kashyap@sachtechsolution.com>
2023-12-01 12:55:05 +0530

    changes

--- server/routes/common.js
+++ server/routes/common.js
@@ -19,7 +19,7 @@ router.get('/getQuestions', verifyTokenBoth, commonController.getQuestions);
 // with admin token routes 
 router.post('/addNewQuestion', validateCSRFToken, verifyTokenAdmin, validate(addQuestionSchema), commonController.addNewQuestion);
 router.post('/updateQuestion', validateCSRFToken, verifyTokenAdmin, validate(updateQuestionSchema), commonController.updateQuestion);
-router.post('/updateCommonContent', validateCSRFToken, verifyTokenAdmin, validate(updateCommonContentSchema), commonController.updateCommonContent);
+router.post('/updateCommonContent', verifyTokenAdmin, validate(updateCommonContentSchema), commonController.updateCommonContent);
 router.get('/fetchZendeskFAQs', commonController.fetchZendeskFAQs);
 router.get('/TwofetchZendeskFAQs', commonController.TwofetchZendeskFAQs);
 
