dcb0e4e75387aab93da1d7eaaddb6b1761fd2751
gauravgit config --global user.emailgit config --global user.name gaurav <gaurav.kashyap@sachtechsolution.com>
2023-12-15 11:09:13 +0530

    remove csrf

--- server/routes/category.js
+++ server/routes/category.js
@@ -10,12 +10,12 @@ const { addCategorySchema, addSubCategorySchema, updateCategorySchema } = requir
 router.get('/getCategories', verifyTokenBoth, categoryController.getCategories);
 
 // with Admin Token routes
-router.post('/addCategory', validateCSRFToken, verifyTokenAdmin, addToMulter.single('category'), validate(addCategorySchema), categoryController.addCategories);
-router.post('/updateCategory', validateCSRFToken, verifyTokenAdmin, addToMulter.single('category'), validate(updateCategorySchema), categoryController.updateCategory);
-router.post('/addSubcategory', validateCSRFToken, verifyTokenAdmin, addToMulter.single('subcategory'), validate(addSubCategorySchema), categoryController.addSubCategories);
-router.post('/updateSubcategory', validateCSRFToken, verifyTokenAdmin, addToMulter.single('subcategory'), validate(updateCategorySchema), categoryController.updateSubcategory);
-router.delete('/deleteCategory/:id', validateCSRFToken, verifyTokenAdmin, categoryController.deleteCategory);
-router.delete('/deleteSubcategory/:id', validateCSRFToken, verifyTokenAdmin, categoryController.deleteSubcategory);
+router.post('/addCategory' , verifyTokenAdmin, addToMulter.single('category'), validate(addCategorySchema), categoryController.addCategories);
+router.post('/updateCategory', verifyTokenAdmin, addToMulter.single('category'), validate(updateCategorySchema), categoryController.updateCategory);
+router.post('/addSubcategory', verifyTokenAdmin, addToMulter.single('subcategory'), validate(addSubCategorySchema), categoryController.addSubCategories);
+router.post('/updateSubcategory', verifyTokenAdmin, addToMulter.single('subcategory'), validate(updateCategorySchema), categoryController.updateSubcategory);
+router.delete('/deleteCategory/:id', verifyTokenAdmin, categoryController.deleteCategory);
+router.delete('/deleteSubcategory/:id', verifyTokenAdmin, categoryController.deleteSubcategory);
 
 // Common Routes
 router.get('*', (req, res) => { res.status(405).json({ status: false, message: "Invalid Get Request" }) });
